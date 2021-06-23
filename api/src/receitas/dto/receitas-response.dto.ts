40c3fcc3e79323c1a797a25af0ed18d18f6b36d8
Eduardo Conti <es.eduardoconti@gmail.com>
2021-06-23 13:58:38 -0300

    alterações

--- api/src/receitas/dto/receitas-response.dto.ts
+++ api/src/receitas/dto/receitas-response.dto.ts
@@ -3,10 +3,10 @@ import { Users } from 'src/users/entity/users.entity';
 
 export class ReceitasResponseDTO {
   id: number;
-  user: string;
+  user: string|Users;
   descricao: string;
   valor: number;
   pagamento: Date;
   pago: boolean;
-  carteira: number;
+  carteira: number|Carteiras;
 }
--- api/src/receitas/dto/receitas.dto.ts
+++ api/src/receitas/dto/receitas.dto.ts
@@ -14,7 +14,6 @@ import { CONSTRAINTS_LIMITS, CONSTRAINTS_MESSAGES } from 'src/shared/constants';
 import { Users } from 'src/users/entity/users.entity';
 
 export class ReceitasDTO {
-  id?: number;
 
   @ApiProperty({
     description: 'uuid do usuario',
